fd28b2b27d81526cecb36e32193a23c24e9ba60a
Admin <abramov.pandos@yandex.ru>
2020-03-14 00:00:02 +0300

    енлыы

---
+++ src/even_numbers.java
@@ -0,0 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class even_numbers {
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        scanner.useLocale(Locale.UK);
+        int x = readint(); int y = readint();
+        int s = 0;
+        while (x<=y){
+            if (x%2==0){
+                s = x;
+                System.out.print(s + " ");
+            }
+            x++;
+        }
+    }
+    public static int readint() {
+        return scanner.nextInt();
+    }
+}
+
